aef3ffd8a9f9178e36d177b1df0c97f6019b3ed9
samaelassal <Sama.ElAssal23d@eslsca.edu.eg>
2026-04-22 14:46:13 +0200

    Part 6

--- Assets/Scripts/ArImageTracker.cs
+++ Assets/Scripts/ArImageTracker.cs
@@ -1,42 +1,58 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
 
+[Serializable]
+public class ImagePrefabEntry {
+public string imageName;
+public GameObject prefab;
+}
 public class ARImageTracker : MonoBehaviour {
-[SerializeField] private ARTrackedImageManager imageManager;
-[SerializeField] private GameObject contentPrefab;
+    [SerializeField] private ARTrackedImageManager imageManager;
+[SerializeField] private List<ImagePrefabEntry> imagePrefabs;
+// Built once at startup for O(1) lookups during tracking
+private Dictionary<string, GameObject> _prefabLookup = new Dictionary<string, GameObject>(); 
 
-private void OnEnable() {
-imageManager.trackablesChanged.AddListener(OnTrackedImagesChanged);
-}
-private void OnDisable() {
-imageManager.trackablesChanged.RemoveListener(OnTrackedImagesChanged);
-}
-
-private void OnTrackedImagesChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
+private void Awake()
     {
+        foreach (var entry in imagePrefabs) {
+            if (!_prefabLookup.ContainsKey(entry.imageName)) {
+                _prefabLookup[entry.imageName] = entry.prefab;
+            }
+        }
+    }
+    private void OnEnable() {
+        imageManager.trackablesChanged.AddListener(OnTrackedImagesChanged);
+    }
+    private void OnDisable() {
+        imageManager.trackablesChanged.RemoveListener(OnTrackedImagesChanged);
+    }
+
+    private void OnTrackedImagesChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs) {
         foreach (var trackedImage in eventArgs.added)
         {
+            string imageName = trackedImage.referenceImage.name;
+            if (_prefabLookup.TryGetValue(imageName, out GameObject prefab))
+            {
             GameObject spawnedContent = Instantiate(
-                contentPrefab, 
+                prefab,
                 trackedImage.transform.position,
-                trackedImage.transform.rotation,
-                trackedImage.transform
+                trackedImage.transform.rotation
             );
             spawnedContent.transform.SetParent(trackedImage.transform);
         }
-        foreach (var trackedImage in eventArgs.updated) {
-            if (trackedImage.transform.childCount > 0)
-            {
-                GameObject content = trackedImage.transform.GetChild(0).gameObject;
-                bool isTracking = trackedImage.trackingState == TrackingState.Tracking;
-                content.SetActive(isTracking);
-            }
+    }
+    foreach (var trackedImage in eventArgs.updated) {
+        if (trackedImage.transform.childCount > 0) {
+            GameObject content = trackedImage.transform.GetChild(0).gameObject;
+            bool isTracking = trackedImage.trackingState == TrackingState.Tracking;
+            content.SetActive(isTracking);
         }
-        foreach (var pair in eventArgs.removed)
-        {
-            ARTrackedImage removedImage = pair.Value;
-            Debug.Log("Image removed: " + removedImage.referenceImage.name);
-        }
-}
+    }
+    foreach (var pair in eventArgs.removed) {
+        Debug.Log("Image removed: " + pair.Value.referenceImage.name);
+    }
+    }
 }
